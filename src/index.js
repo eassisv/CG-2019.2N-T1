21cda4612e8f8479d2790d59ece01f45a3795890
Éverton Assis <eassis.vieira@mail.com>
2019-12-15 19:08:46 -0300

    Update loading models

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+import {TGALoader} from 'three/examples/jsm/loaders/TGALoader';
 import glbGlobo from '../assets/models/globo/scene.glb';
-import gltfPokeball from '../assets/models/pokeball/scene.gltf';
-import '../assets/models/pokeball/scene.bin';
+import fbxPokeball from '../assets/models/pokeball-fbx/pokeball.fbx';
 
 let scene, camera, renderer, light;
-let pokeball, globo;
+let pokeball, globo, group;
 
 (() => {
+  THREE.Loader.Handlers.add(/\.tga$/i, new TGALoader());
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
     75,
@@ -15,9 +18,8 @@ let pokeball, globo;
     0.1,
     1000
   );
-
+  camera.position.z = 900;
   scene.add(camera);
-  camera.position.z = 40;
 
   renderer = new THREE.WebGLRenderer({alpha: 1});
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,46 +28,42 @@ let pokeball, globo;
   document.body.appendChild(renderer.domElement);
   window.addEventListener('resize', onWindowResize);
 
-  {
-    light = new THREE.DirectionalLight(0xffffff, 1);
-    scene.add(light);
-    light.position.z = 30;
-  }
+  group = new THREE.Group();
+  scene.add(group);
+  setLight();
+  loadPokeball();
+  // loadGlobo();
 
-  {
-    const loader = new GLTFLoader();
-    loader.load(
-      gltfPokeball,
-      gltf => {
-        pokeball = gltf.scene;
-        scene.add(gltf.scene);
-      },
-      undefined,
-      error => {
-        console.log('Error: Pokeball', error);
-      }
-    );
-  }
-
-  {
-    const loader = new GLTFLoader();
-    loader.load(
-      glbGlobo,
-      gltf => {
-        globo = gltf.scene;
-        animate();
-        scene.add(globo);
-      },
-      undefined,
-      error => {
-        console.log('Error: Globo', error);
-      }
-    );
-  }
-
-  animate();
   initEvents();
+  animate();
 })();
+
+function setLight() {
+  light = new THREE.DirectionalLight(0xffffff, 2.5);
+  light.position.set(camera.position.x, camera.position.y, camera.position.z);
+  light.target = group;
+  scene.add(light);
+}
+
+function loadGlobo() {
+  const loader = new GLTFLoader();
+  loader.load(glbGlobo, gltf => {
+    console.log('globo:', gltf);
+    globo = gltf.scene;
+    globo.scale.set(10, 10, 10);
+    group.add(globo, new THREE.PointLight(0xbb00ff));
+  });
+}
+
+function loadPokeball() {
+  const loader = new FBXLoader();
+  loader.load(fbxPokeball, fbx => {
+    pokeball = fbx;
+    console.log('pokeball:', pokeball);
+    // pokeball.scale.set(0.05, 0.05, 0.05);
+    group.add(pokeball);
+  });
+}
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -77,7 +75,8 @@ function initEvents() {
   document.addEventListener(
     'wheel',
     e => {
-      camera.zoom += 0.01;
+      camera.zoom += e.deltaY * 0.01;
+      camera.updateProjectionMatrix();
     },
     false
   );
